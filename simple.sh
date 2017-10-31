498ae83ad8382a114b54c939eb68a76b57c6bcf6
Ramcharan Sudarsanam <sudarsar@submit-em64t-01.hpc.engr.oregonstate.edu>
2017-10-31 15:59:48 -0700

    changed all floats to double

---
+++ simple.sh
@@ -0,0 +1,27 @@
+#!/bin/csh
+
+# use current working directory for input and output
+# default is to use the users home directory
+#$ -cwd
+
+# name this job
+#$ -N indep-method
+
+# send stdout and stderror to this file
+#$ -o submit-output.out
+#$ -j y
+
+# select queue - if needed 
+#$ -q eecs,eecs2,share,share2,share3,share4
+
+# see where the job is being run
+hostname
+
+# print date and time
+date
+
+# Sleep for 10 seconds
+make all
+
+# print date and time again
+date
